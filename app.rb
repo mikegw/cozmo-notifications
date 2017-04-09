17c01d28506ad25d9185d64820845737ad108df4
Mike Williamson <mike@cardflight.com>
2017-04-09 16:34:57 -0400

    Add ids

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ Bundler.require
 
 require 'json'
 require 'logger'
+require 'securerandom'
 
 notifications = []
 LOGGER = Logger.new(STDOUT)
@@ -14,6 +15,14 @@ end
 
 post '/' do
   notification = JSON.parse(request.body.read)
+  notification['id'] = SecureRandom.hex
   notifications << notification
   status 201
+end
+
+delete '/:id' do |id|
+  notification = notifications.find { |notification| notification['id'] == id }
+  halt 404 unless notification
+  notifications.delete(notification)
+  status 200
 end
